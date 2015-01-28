2cad15f465a5573d06973d7d2f54f0cec5f7e7d2
王家伟 <572264287@qq.com>
2015-01-28 14:30:53 +0800

    Create show_Oct_window.c

---
+++ show_Oct_window.c
@@ -0,0 +1,43 @@
+#include<gtk/gtk.h>
+void show_Oct_window()
+{
+ gtk_button_set_label (GTK_BUTTON (button1), " ");   /*按钮pi显示为空*/
+ gtk_widget_show (button1);
+ gtk_button_set_label (GTK_BUTTON (button2), " ");   /*按钮sin显示为空*/
+ gtk_widget_show (button2);
+ gtk_button_set_label (GTK_BUTTON (button3), " ");   /*按钮cos显示为空*/
+ gtk_widget_show (button3);
+ gtk_button_set_label (GTK_BUTTON (button4), " ");   /*按钮tan显示为空*/
+ gtk_widget_show (button4);
+ gtk_button_set_label (GTK_BUTTON (button6), " ");   /*按钮Exp显示为空*/
+ gtk_widget_show (button6);
+ gtk_button_set_label (GTK_BUTTON (button14), "7");  /*数字按钮7需要显示*/
+ gtk_widget_show (button14);
+ gtk_button_set_label (GTK_BUTTON (button15), "4");  /*数字按钮4需要显示*/
+ gtk_widget_show (button15);
+ gtk_button_set_label (GTK_BUTTON (button18), " ");  /*数字按钮A显示为空*/
+ gtk_widget_show (button18);
+ gtk_button_set_label (GTK_BUTTON (button19), " ");  /*数字按钮8显示为空*/
+ gtk_widget_show (button19);
+ gtk_button_set_label (GTK_BUTTON (button20), "5");  /*数字按钮5需要显示*/
+ gtk_widget_show (button20);
+ gtk_button_set_label (GTK_BUTTON (button21), "2");  /*数字按钮2需要显示*/
+ gtk_widget_show (button21);
+ gtk_button_set_label (GTK_BUTTON (button23), " ");  /*数字按钮B显示为空*/
+ gtk_widget_show (button23);
+ gtk_button_set_label (GTK_BUTTON (button24), " ");  /*数字按钮9显示为空*/
+ gtk_widget_show (button24);
+ gtk_button_set_label (GTK_BUTTON (button25), "6");  /*数字按钮6需要显示*/
+ gtk_widget_show (button25);
+ gtk_button_set_label (GTK_BUTTON (button26), "3");  /*数字按钮3需要显示*/
+ gtk_widget_show (button26);
+ gtk_button_set_label (GTK_BUTTON (button28), " "); /*数字按钮C显示为空*/
+ gtk_widget_show (button28);
+ gtk_button_set_label (GTK_BUTTON (button33), " "); /*数字按钮D显示为空*/
+ gtk_widget_show (button33);
+ gtk_button_set_label (GTK_BUTTON (button38), " "); /*数字按钮E显示为空*/
+ gtk_widget_show (button38);
+ gtk_button_set_label (GTK_BUTTON (button42), " "); /*数字按钮F显示为空*/
+ gtk_widget_show (button42);
+ }
+ 
