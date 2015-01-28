ce3a0a353bd01fac4bc77969870d80c5176aeb37
王家伟 <572264287@qq.com>
2015-01-28 14:30:09 +0800

    Create show_Hex_window.c

---
+++ show_Hex_window.c
@@ -0,0 +1,42 @@
+#include<gtk/gtk.h>
+void show_Hex_window()
+{
+ gtk_button_set_label (GTK_BUTTON (button1), " ");  /*按钮pi显示为空*/
+ gtk_widget_show (button1);
+ gtk_button_set_label (GTK_BUTTON (button2), " ");  /*按钮sin显示为空*/
+ gtk_widget_show (button2);
+ gtk_button_set_label (GTK_BUTTON (button3), " "); /*按钮cos显示为空*/
+ gtk_widget_show (button3);
+ gtk_button_set_label (GTK_BUTTON (button4), " "); /*按钮tan显示为空*/
+ gtk_widget_show (button4);
+ gtk_button_set_label (GTK_BUTTON (button6), " "); /*按钮Exp显示为空*/
+ gtk_widget_show (button6);
+ gtk_button_set_label (GTK_BUTTON (button14), "7"); /*数字按钮7需要显示*/
+ gtk_widget_show (button14);
+ gtk_button_set_label (GTK_BUTTON (button15), "4"); /*数字按钮4需要显示*/
+ gtk_widget_show (button15);
+ gtk_button_set_label (GTK_BUTTON (button18), "A"); /*数字按钮A需要显示*/
+ gtk_widget_show (button18);
+ gtk_button_set_label (GTK_BUTTON (button19), "8"); /*数字按钮8需要显示*/
+ gtk_widget_show (button19);
+ gtk_button_set_label (GTK_BUTTON (button20), "5"); /*数字按钮5需要显示*/
+ gtk_widget_show (button20);
+ gtk_button_set_label (GTK_BUTTON (button21), "2"); /*数字按钮2需要显示*/
+ gtk_widget_show (button21);
+ gtk_button_set_label (GTK_BUTTON (button23), "B"); /*数字按钮B需要显示*/
+ gtk_widget_show (button23);
+ gtk_button_set_label (GTK_BUTTON (button24), "9"); /*数字按钮9需要显示*/
+ gtk_widget_show (button24);
+ gtk_button_set_label (GTK_BUTTON (button25), "6"); /*数字按钮6需要显示*/
+ gtk_widget_show (button25);
+ gtk_button_set_label (GTK_BUTTON (button26), "3"); /*数字按钮3需要显示*/
+ gtk_widget_show (button26);
+ gtk_button_set_label (GTK_BUTTON (button28), "C"); /*数字按钮C需要显示*/
+ gtk_widget_show (button28);
+ gtk_button_set_label (GTK_BUTTON (button33), "D"); /*数字按钮D需要显示*/
+ gtk_widget_show (button33);
+ gtk_button_set_label (GTK_BUTTON (button38), "E"); /*数字按钮E需要显示*/
+ gtk_widget_show (button38);
+ gtk_button_set_label (GTK_BUTTON (button42), "F"); /*数字按钮F需要显示*/
+ gtk_widget_show (button42);
+}
